4488ee24143efa0a716959ef9b2942733f64c1d3
Samantha Barros <samantha.barros95@gmail.com>
2020-06-15 18:47:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,7 @@ class TicTacToe
     @board = Array.new(9, " ")
   end 
   
-  WIN_COMBINATIONS[
+  WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
     [6,7,8],
@@ -13,7 +13,7 @@ class TicTacToe
     [2,5,8],
     [0,4,8],
     [2,4,6]
-    ]
+  ]
   
   
 end 
