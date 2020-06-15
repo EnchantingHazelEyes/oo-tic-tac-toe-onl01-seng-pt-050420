7decf7377ffea997988d5e8aee27214e5bf02c1f
Samantha Barros <samantha.barros95@gmail.com>
2020-06-15 18:52:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,16 +4,17 @@ class TicTacToe
     @board = Array.new(9, " ") #new board setup 
   end 
   
-  WIN_COMBINATIONS = [ #All possible combinations
-    [0,1,2], #Top 
-    [3,4,5], #Middle
-    [6,7,8], #Bottom
-    [0,3,6], #Left Col
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+  WIN_COMBINATIONS = [ #Array with all possible combinations
+    [0,1,2], #Top Row 
+    [3,4,5], #Middle Row 
+    [6,7,8], #Bottom Row 
+    [0,3,6], #Left Column
+    [1,4,7], #Middle Column
+    [2,5,8], #Right Column
+    [0,4,8], #Diagonal Left-to-right
+    [2,4,6]  #Diagonal Right-to-left
   ]
+  
   
   
 end 
