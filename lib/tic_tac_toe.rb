b5568e31e0d78976e1544f1a30c37b01840a3f0f
Samantha Barros <samantha.barros95@gmail.com>
2020-06-15 19:24:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,4 +47,17 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   
+  def turn
+    puts "Player #{current_player}, please enter a number 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    cp = current_player
+    if valid_move?(index)
+      move(index, cp)
+      display_board
+    else
+      turn
+    end
+  end
+  
 end 
